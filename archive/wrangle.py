088303c84c50fe6be7bd6a09911df5d9a43d423e
Joshua Wheeler <joshua.r.wheeler.27@gmail.com>
2022-03-09 09:43:58 -0600

    moved to archive

---
+++ archive/wrangle.py
@@ -0,0 +1,74 @@
+import os
+import numpy as np
+import pandas as pd
+import acquire
+import prepare
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+'''
+This module uses the acquire and prepare modules to create a usable NPR corpus.
+
+Any further wrangling can be added here.
+'''
+
+
+def get_npr_data():
+    '''
+    This function reads in the NPR corpus data, cleans, and prepares it then 
+    writes data to a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('../npr_corpus.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('../npr_corpus.csv', index_col=0)      
+        # print statements for size of each df
+        print(f'The df has {df.shape[0]} rows and {df.shape[1]} columns.')
+
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = acquire.get_df()
+
+        # prepare data
+        df = prepare.prep_npr_data(df)
+
+        # Cache data
+        df.to_csv('npr_corpus.csv')
+
+        # print statements for size of each df
+        print(f'The df has {df.shape[0]} rows and {df.shape[1]} columns.')
+    df.drop(columns='episode_date', inplace = True)
+    #change name of a couple columns:
+    df = df.rename(columns={'episode_id':"story_id_num", 'episode_order':'utterance_order'})
+    
+    # regex to adjust speaker names
+    df['speaker'] = df.speaker.str.replace(r'\([^)]*\)','', regex=True)
+    df['speaker'] = df.speaker.str.replace(r'host','', regex=True)
+    df['speaker'] = df.speaker.str.replace(r'[^a-z0-9\s\.]','', regex=True).str.strip()
+    df['speaker'] = df.speaker.str.replace('lourdes','lulu')
+
+    ## redo is_host
+    # create host_map and list of hosts
+    host_map = pd.read_json('../host-map.json')
+    host_map = host_map.T
+    hosts = host_map.name.to_list()
+    hosts.append('neal conan')
+    df['is_host'] = df.speaker.isin(hosts)
+    
+    #drop nulls
+    df.dropna(inplace = True)
+    
+    return df
+
+def split_data(df):
+    '''
+    Takes in a dataframe and returns train, validate, test subset dataframes. 
+    '''
+    # tfidf = TfidfVectorizer()
+    # X = tfidf.fit_transform(df.lemmatized)
+    # y = df.language
+    train, test = train_test_split(df, test_size = .2, stratify = df.is_host, random_state = 222)
+    train, validate = train_test_split(train, test_size = .3, stratify = train.is_host,random_state = 222)
+    return train, validate, test
---
+++ cluster.py
@@ -0,0 +1,58 @@
+from sklearn.cluster import KMeans
+from sklearn.model_selection import learning_curve
+from sklearn.preprocessing import MinMaxScaler
+
+def min_max_scaler(train, valid, test):
+    '''
+    Uses the train & test datasets created by the split_my_data function
+    Returns 3 items: mm_scaler, train_scaled_mm, test_scaled_mm
+    This is a linear transformation. Values will lie between 0 and 1
+    '''
+    num_vars = list(train.select_dtypes('number').columns)
+    scaler = MinMaxScaler(copy=True, feature_range=(0,1))
+    train[num_vars] = scaler.fit_transform(train[num_vars])
+    valid[num_vars] = scaler.transform(valid[num_vars])
+    test[num_vars] = scaler.transform(test[num_vars])
+    return scaler, train, valid, test
+
+
+df2 = df.sample(10_000, random_state = 222)
+
+train, validate, test = wrangle.split_data(df2)
+scaler, train_scaled, validate_scaled, test_scaled = min_max_scaler(train, validate, test)
+# define features for clustering
+X_train_cluster = train_scaled[['vader', 'word_count','question_mark_count']]
+
+#repeat for validate and test
+X_validate_cluster = validate_scaled[['vader', 'word_count','question_mark_count']]
+X_test_cluster = test_scaled[['vader', 'word_count','question_mark_count']]
+
+# define cluster object
+kmeans = KMeans(n_clusters=4, random_state = 333)
+# fit cluster object to features
+kmeans.fit(X_train_cluster)
+# use the object
+kmeans.predict(X_train_cluster)
+
+# add cluster features to train and X_train df's
+train_scaled['cluster'] = kmeans.predict(X_train_cluster)
+validate_scaled['cluster'] = kmeans.predict(X_validate_cluster)
+test_scaled['cluster'] = kmeans.predict(X_test_cluster)
+X_train_cluster['cluster'] = kmeans.predict(X_train_cluster)
+
+from mpl_toolkits.mplot3d import Axes3D
+# %matplotlib notebook
+# plt.figure(figsize = (14,14))
+fig = plt.figure(figsize = (14,14))
+ax = fig.add_subplot(111, projection = '3d')
+
+x = X_train_cluster.word_count
+y = X_train_cluster.vader
+z = X_train_cluster.question_mark_count
+ax.scatter(x,y,z, c=X_train_cluster.cluster, s = 40, cmap = 'jet')
+# ax.legend()
+ax.set_xlabel('word_count', fontsize = 15)
+ax.set_ylabel('vader',fontsize = 15)
+ax.set_zlabel('question_mark_count',fontsize = 15)
+plt.title('NPR Clusters by Sentiment, Word Count, and Question Marks')
+plt.show()
